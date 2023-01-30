1f70cdba010f567aa77174edfd7891479629ca86
Orlando Hernandez <ohernandez@accoes.com>
2023-01-30 12:02:24 -0800

    Add project files.

---
+++ RAA_Level2/Forms/MyForm.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace RAA_Level2
+{
+    /// <summary>
+    /// Interaction logic for Window.xaml
+    /// </summary>
+    public partial class MyForm : Window
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = @"C:\";
+            openFile.Filter = "csv files (*.csv)|*.csv";
+
+            if (openFile.ShowDialog() == true)
+            {
+                txbInput.Text = openFile.FileName;
+            }
+            else
+            {
+                txbInput.Text = "";
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
